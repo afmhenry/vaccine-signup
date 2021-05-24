2c26c18032ffb05546237130debff1e413d2f69e
Alexander Henry <manebjaelke@Alexanders-MacBook-Air.local>
2021-05-24 19:56:25 +0200

    removed hardcoded wait, implict used

--- vaccine.py
+++ vaccine.py
@@ -8,7 +8,6 @@ from collections import OrderedDict
 
 #radio selection of vaccine site, number 1-9 as string
 area = "6"
-wait_time = .5
 
 print("Starting now: "+str(datetime.now()))
 
@@ -19,40 +18,35 @@ with open('/Users/manebjaelke/Documents/vaccine-signup/data.json') as json_file:
         #url for the form
         driver.get("https://www.survey-xact.dk/LinkCollector?key=JPH8ZGHNL21N")
 
-        #hacky time delays to avoid async messiness. 
-        time.sleep(1)
+        #Less hacky implicit wait so no hardcoded wait time needed. 
+        driver.implicitly_wait(10)
+
         firstNext = driver.find_element_by_class_name("next-area")
         firstNext.click()
-        time.sleep(wait_time)
 
         #iterate over the first pages and put in values.
         for entry in answer_dict:
             input_field = driver.find_element_by_name(entry['type'])
             next_button = driver.find_element_by_class_name("next-area")
-
-            time.sleep(wait_time)
             input_field.send_keys(entry['value'])
-            time.sleep(wait_time)
             next_button.click()
-            time.sleep(wait_time)
 
         #radio needs weird stuff, so had to do seperate. 
         input_field = driver.find_element_by_css_selector("input[type='radio'][data-choice-label-value='"+area+"']")
         input_field.find_element_by_xpath('..').click()
         next_button = driver.find_element_by_class_name("next-area")
         next_button.click()
-        time.sleep(wait_time)
 
         #data acceptance confirmation page
         next_button = driver.find_element_by_class_name("next-area")
         next_button.click()
-        time.sleep(wait_time)
 
         #submission page
         #Commented so you don't submit without testing first
-        #next_button = driver.find_element_by_class_name("next-area")
-        #next_button.click()
-        time.sleep(10)
+        next_button = driver.find_element_by_class_name("next-area")
+        next_button.click()
+        print("True if completed:" + driver.title == "Region Hovedstaden")
+
 print("Ending now: "+str(datetime.now())+"\n")
 
 
